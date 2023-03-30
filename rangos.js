ceb4203e5ba00386dba3a45e4035af2bb9edd4a7
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-03-30 09:59:57 -0500

    "punto 3 ok"

---
+++ rangos.js
@@ -0,0 +1,9 @@
+limiteSup = 10 
+limiteInf = 2
+ingresado = 11
+
+if(ingresado > limiteInf && ingresado <limiteSup ){
+    console.log("el numero "+ ingresado + " ingresado SI esta dentro del rango");
+}else{
+    console.log("el numero  "+ ingresado + " ingresado NO esta dentro del rango");
+}
