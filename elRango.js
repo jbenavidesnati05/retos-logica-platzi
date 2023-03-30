4eaf02f4f29c6d03bea92e687c9708e4c44e1cab
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-03-30 09:54:01 -0500

    "reto dos ok"

---
+++ elRango.js
@@ -0,0 +1,8 @@
+limite = 4 
+ingresado = 4
+
+if(limite >ingresado){
+    console.log("el numero "+ ingresado + " ingresado SI esta dentro del rango");
+}else{
+    console.log("el numero  "+ ingresado + " ingresado NO esta dentro del rango");
+}
