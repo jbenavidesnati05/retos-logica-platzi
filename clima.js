5cd6109396d842f7c25f246423452a3e07e037b7
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-03-31 12:58:59 -0500

    "clima ok"

---
+++ clima.js
@@ -0,0 +1,15 @@
+
+let lloviendo = "si"
+let viento = "si"
+
+if(lloviendo == "si"){
+    console.log("lloviendo");
+    if(viento == "si"){
+        console.log("viento");
+        console.log("Quedese en casa");
+    }else{
+        console.log("lleve sombriña");
+    }
+}else{
+    console.log("que tenga un bonito dia");
+}
